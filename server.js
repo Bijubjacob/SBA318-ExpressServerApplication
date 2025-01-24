76f507bac76ac6da9cdd7a5402d5802491647be0
Bijubjacob <bijubjacob@gmail.com>
2025-01-23 18:03:31 -0600

    second commit

--- server.js
+++ server.js
@@ -15,28 +15,35 @@ app.get("/api/users", (req, res) => {
     res.json(users);
 });
 
-// Creating a simple GET route for individual users,
-// using a route parameter for the unique id.
-app.get("/api/users/:id", (req, res) => {
+app.get("/api/users/:id", (req, res, next) => {
     const user = users.find((u) => u.id == req.params.id);
     if (user) res.json(user);
+    else next();
 });
 
-// Creating a GET route for the entire posts database.
-// This would be impractical in larger data sets.
 app.get("/api/posts", (req, res) => {
     res.json(posts);
 });
 
-// Creating a simple GET route for individual posts,
-// using a route parameter for the unique id.
-app.get("/api/posts/:id", (req, res) => {
+app.get("/api/posts/:id", (req, res, next) => {
     const post = posts.find((p) => p.id == req.params.id);
     if (post) res.json(post);
+    else next();
 });
 
 app.get("/", (req, res) => {
     res.send("Work in progress!");
+});
+
+
+// Custom 404 (not found) middleware.
+// Since we place this last, it will only process
+// if no other routes have already sent a response!
+// We also don't need next(), since this is the
+// last stop along the request-response cycle.
+app.use((req, res) => {
+    res.status(404);
+    res.json({ error: "Resource Not Found" });
 });
 
 app.listen(port, () => {
