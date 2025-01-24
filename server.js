9c0a2ab5d645be1894ed450ab6c6e4fbaf0b6134
Bijubjacob <bijubjacob@gmail.com>
2025-01-23 18:15:19 -0600

    third commit

--- server.js
+++ server.js
@@ -1,46 +1,69 @@
-// -------------------------[Requires/Imports]
 const express = require("express");
-// -->brings it in
+const bodyParser = require("body-parser");
+
+// These are now route imports, not database imports!
+const users = require("./routes/users");
+const posts = require("./routes/posts");
+
 const app = express();
-// --> init it
 const port = 3000;
 
-// Importing the data from our fake database files.
-const users = require("./data/users");
-const posts = require("./data/posts");
+// Parsing Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
 
-// Creating a GET route for the entire users database.
-// This would be impractical in larger data sets.
-app.get("/api/users", (req, res) => {
-    res.json(users);
+// Logging Middlewaare
+app.use((req, res, next) => {
+    const time = new Date();
+
+    console.log(
+        `-----
+${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+    );
+    if (Object.keys(req.body).length > 0) {
+        console.log("Containing the data:");
+        console.log(`${JSON.stringify(req.body)}`);
+    }
+    next();
 });
 
-app.get("/api/users/:id", (req, res, next) => {
-    const user = users.find((u) => u.id == req.params.id);
-    if (user) res.json(user);
-    else next();
+// Valid API Keys.
+apiKeys = ["perscholas", "ps-example", "hJAsknw-L198sAJD-l3kasx"];
+
+// New middleware to check for API keys!
+// Note that if the key is not verified,
+// we do not call next(); this is the end.
+// This is why we attached the /api/ prefix
+// to our routing at the beginning!
+app.use("/api", function (req, res, next) {
+    var key = req.query["api-key"];
+
+    // Check for the absence of a key.
+    if (!key) {
+        res.status(400);
+        return res.json({ error: "API Key Required" });
+    }
+
+    // Check for key validity.
+    if (apiKeys.indexOf(key) === -1) {
+        res.status(401);
+        return res.json({ error: "Invalid API Key" });
+    }
+
+    // Valid key! Store it in req.key for route access.
+    req.key = key;
+    next();
 });
 
-app.get("/api/posts", (req, res) => {
-    res.json(posts);
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-    const post = posts.find((p) => p.id == req.params.id);
-    if (post) res.json(post);
-    else next();
-});
+// Use our Routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
 
 app.get("/", (req, res) => {
     res.send("Work in progress!");
 });
 
-
-// Custom 404 (not found) middleware.
-// Since we place this last, it will only process
-// if no other routes have already sent a response!
-// We also don't need next(), since this is the
-// last stop along the request-response cycle.
+// 404 Middleware
 app.use((req, res) => {
     res.status(404);
     res.json({ error: "Resource Not Found" });
